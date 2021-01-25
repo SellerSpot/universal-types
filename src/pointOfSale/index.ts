02fe9084b4198701109d9416b8f947b58846cac2
Rohit Raj <rij7u7d@gmail.com>
2021-01-25 19:57:24 +0530

    Added index entries

--- src/pointOfSale/index.ts
+++ src/pointOfSale/index.ts
@@ -1,2 +1,8 @@
 export * as brandRequestTypes from './Brand/brand.request.types';
 export * as brandResponseTypes from './Brand/brand.response.types';
+
+export * as categoryRequestTypes from './Category/category.request.types';
+export * as categoryResponseTypes from './Category/category.response.types';
+
+export * as taxBracketRequestTypes from './TaxBracket/taxBracket.request.types';
+export * as taxBracketResponseTypes from './TaxBracket/taxBracket.response.types';
