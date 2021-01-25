f96b0a95f9226cd5acfc7d0e398cdd524ad138ca
Rohit Raj <rij7u7d@gmail.com>
2021-01-26 00:39:44 +0530

    Upgraded Brands Types

--- src/pointOfSale/Brand/brand.response.types.ts
+++ src/pointOfSale/Brand/brand.response.types.ts
@@ -1,11 +1,32 @@
 import { tenantDbModels } from '@sellerspot/database-models';
 import { IResponse } from 'utils';
 
+// field names for textFields involved in this API
+type fieldNames = 'name';
+
 /**
  * Response when brand is fetched from server
  */
 
 export type IGetBrand = IResponse & {
+    data?: tenantDbModels.pointOfSaleModels.BrandModel.IBrandSchema;
+    error?: string;
+};
+
+/**
+ * Response when a new brand is created
+ */
+export type ICreateBrand = IResponse & {
+    data?: tenantDbModels.pointOfSaleModels.BrandModel.IBrandSchema;
+    error?: {
+        [k in fieldNames]?: string;
+    };
+};
+
+/**
+ * Response when a brand is deleted
+ */
+export type IDeleteBrand = IResponse & {
     data?: tenantDbModels.pointOfSaleModels.BrandModel.IBrandSchema;
     error?: string;
 };
