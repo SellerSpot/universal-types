886cf0f692d6694ef9a4444dbf1d56f58a0ede7f
Rohit Raj <rij7u7d@gmail.com>
2021-01-26 00:48:09 +0530

    Upgraded Sales API Types

--- src/pointOfSale/Sale/sale.response.types.ts
+++ src/pointOfSale/Sale/sale.response.types.ts
@@ -1,6 +1,26 @@
 import { tenantDbModels } from '@sellerspot/database-models';
+import { IResponse } from 'utils';
 
 /**
  * Response when a sale is fetched from server
  */
-export type IGetSale = tenantDbModels.pointOfSaleModels.SaleModel.ISaleSchema;
+export type IGetSale = IResponse & {
+    data?: tenantDbModels.pointOfSaleModels.SaleModel.ISaleSchema;
+    error?: string;
+};
+
+/**
+ * Response when a new sale is created
+ */
+export type ICreateSale = IResponse & {
+    data?: tenantDbModels.pointOfSaleModels.SaleModel.ISaleSchema;
+    error?: string;
+};
+
+/**
+ * Response when a sale is deleted
+ */
+export type IDeleteSale = IResponse & {
+    data?: tenantDbModels.pointOfSaleModels.SaleModel.ISaleSchema;
+    error?: string;
+};
