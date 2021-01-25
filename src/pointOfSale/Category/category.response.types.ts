e599bf7f15481a5ef62093ece694bf976f1b1726
Rohit Raj <rij7u7d@gmail.com>
2021-01-26 00:43:18 +0530

    Upgraded Category APis

--- src/pointOfSale/Category/category.response.types.ts
+++ src/pointOfSale/Category/category.response.types.ts
@@ -1,6 +1,31 @@
 import { tenantDbModels } from '@sellerspot/database-models';
+import { IResponse } from 'utils';
+
+// field names for textFields involved in this API
+type fieldNames = 'name';
 
 /**
  * Response when category is fetched from server
  */
-export type IGetCategory = tenantDbModels.pointOfSaleModels.CategoryModel.ICategorySchema;
+export type IGetCategory = IResponse & {
+    data?: tenantDbModels.pointOfSaleModels.CategoryModel.ICategorySchema;
+    error?: string;
+};
+
+/**
+ * Response when a new category is created
+ */
+export type ICreateCategory = IResponse & {
+    data?: tenantDbModels.pointOfSaleModels.CategoryModel.ICategorySchema;
+    error?: {
+        [k in fieldNames]?: string;
+    };
+};
+
+/**
+ * Response when a new category is created
+ */
+export type IDeleteCategory = IResponse & {
+    data?: tenantDbModels.pointOfSaleModels.CategoryModel.ICategorySchema;
+    error?: string;
+};
