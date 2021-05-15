044ce8d93386688fd045add661153708862053b3
Immanuel Irwin <noelimmanuel1998@gmail.com>
2021-05-15 19:07:39 +0530

    Added Category Rotes

--- src/catalogue/index.ts
+++ src/catalogue/index.ts
@@ -1,2 +1,6 @@
 // routes exports
 export * as catalogueRoutes from './routes';
+
+//category export
+export * from './category/request';
+export * from './category/response';
--- src/catalogue/routes.ts
+++ src/catalogue/routes.ts
@@ -1,29 +1,56 @@
-export enum CATALOGUE {
-    /**
-     * @method GET
-     */
-    LIST_CATAGORY = '/categories',
-    /**
-     * @method GET
-     * @pathparam catagoryid
-     */
-    GET_CATAGORY = '/categories:id',
-    /**
-     * @method POST
-     * @type {} requestBody
-     * @type {} responseBody
-     */
-    CREATE_CATAGORY = '/categories',
-    /**
-     * @method PUT
-     * @pathparam catagoryid
-     * @type {} requestBody
-     * @type {} responseBody
-     */
-    EDIT_CATAGORY = '/categories:id',
-    /**
-     * @method DELETE
-     * @pathparam catagoryid
-     */
-    DELETE_CATAGORY = '/categories:id',
-}
+export const CATALOGUE = {
+    CATAGORY: {
+        /**
+         * common namespace that must be appened before all request
+         */
+        RESOURCE: '/categories',
+        /**
+         * All available rest actions for catagory
+         */
+        ACTION: {
+            /**
+             * @method GET
+             * @type {IListCategoryResponse} responseBody
+             */
+            LIST: '/',
+            /**
+             * @method GET
+             * @pathparam catagoryid
+             * @type {IGetCategoryResponse} responseBody
+             */
+            GET: '/:id',
+            /**
+             * @method POST
+             * @type {ICreateCategoryRequest} requestBody
+             * @type {ICreateCategoryResponse} responseBody
+             */
+            CREATE: '/',
+            /**
+             * @method PUT
+             * @pathparam catagoryid
+             * @type {IEditCategoryRequest} requestBody
+             * @type {IEditCategoryResponse} responseBody
+             */
+            EDIT: '/:id',
+            /**
+             * @method PUT
+             * @pathparam {catagoryid} parent id of the sibling should be sent
+             * @type {IEditCategorySiblingOrderRequest} requestBody
+             * @type {IEditCategorySiblingOrderResponse} responseBody
+             */
+            EDIT_SIBLING_ORDER: '/:id/siblingorder',
+            /**
+             * @method PUT
+             * @pathparam catagoryid
+             * @type {IEditCategoryPositionRequest} requestBody
+             * @type {IEditCategoryPositionResponse} responseBody
+             */
+            EDIT_CATEGORY_POSITION: '/:id/position',
+            /**
+             * @method DELETE
+             * @pathparam catagoryid
+             */
+            DELETE: '/:id',
+        },
+    },
+};
