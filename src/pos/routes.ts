df518a58b96e855f2604c355759c387fefde998e
Immanuel Irwin <noelimmanuel1998@gmail.com>
2021-06-01 00:49:10 +0530

    Added Category Routes

--- src/pos/routes.ts
+++ src/pos/routes.ts
@@ -1,6 +1,3 @@
-/**
- * Holds the major routes for the server
- */
 export enum POS {
     /**
      * pos service heartbeat
@@ -8,4 +5,31 @@ export enum POS {
      * @request  GET
      */
     INFO = '/info',
+
+    /** Inventory API starts here */
+    /**
+     * @description Product addition to inventory. Specify **productId** (maps inventory details with existing product) or **product** (creates new product and maps inventory details)
+     * @method POST
+     */
+    ADD_TO_INVENTORY = '/inventories',
+
+    /**
+     * @description list all products in inventory
+     * @method GET
+     */
+    INVENTORY_LIST = '/inventories',
+
+    /**
+     * @description update product - inventory details
+     * @method PUT
+     */
+    INVENTORY_EDIT = '/inventories/:id',
+
+    /**
+     * @description delete product from inventory
+     * @method DELETE
+     */
+    INVENTORY_DELETE = '/inventories',
+
+    /** Inventory API ends here */
 }
