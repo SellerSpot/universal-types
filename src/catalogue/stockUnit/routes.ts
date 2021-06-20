5d6fe15e5b4acd86e8841b4b741ebf1e02d31886
ThayalanGR <grthayalan18@gmail.com>
2021-06-20 20:06:10 +0530

    stockunit req res body typings in routes added

--- src/catalogue/stockUnit/routes.ts
+++ src/catalogue/stockUnit/routes.ts
@@ -1,23 +1,39 @@
+import { ICreateStockUnitRequest, IEditStockUnitRequest } from './request';
+import {
+    ICreateStockUnitResponse,
+    IDeleteStockUnitResponse,
+    IEditStockUnitResponse,
+    IGetAllStockUnitResponse,
+    IGetStockUnitResponse,
+} from './response';
+
 export enum STOCK_UNIT {
     /**
      * @method GET
+     * @type {IGetStockUnitResponse} - responseBody
      */
     GET = '/stockunits/:id',
     /**
      * @method GET
+     * @type {IGetAllStockUnitResponse} - responseBody
      */
     GET_ALL = '/stockunits',
     /**
-      /**
-       * @method POST
-       */
+     /**
+      * @method POST
+      * @type {ICreateStockUnitRequest} - requestBody
+      * @type {ICreateStockUnitResponse} - responseBody
+      */
     CREATE = '/stockunits',
     /**
      * @method PUT
+     * @type {IEditStockUnitRequest} - requestBody
+     * @type {IEditStockUnitResponse} - responseBody
      */
     EDIT = '/stockunits/:id',
     /**
      * @method DELETE
+     * @type {IDeleteStockUnitResponse} - responseBody
      */
     DELETE = '/stockunits/:id',
 }
