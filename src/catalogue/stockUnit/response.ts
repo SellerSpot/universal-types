e5343f5f1fb42fe322ca15dc7eea0ad25c14cc04
Rohit Raj <rij7u7d@gmail.com>
2021-06-20 15:46:13 +0530

    Added types for temporary stability

--- src/catalogue/stockUnit/response.ts
+++ src/catalogue/stockUnit/response.ts
@@ -6,14 +6,15 @@ export interface IStockUnitData {
     isDefault: boolean;
 }
 
+interface IStockUnitRespone extends IResponse {
+    data: IStockUnitData;
+}
+
 export interface IGetAllStockUnitResponse extends IResponse {
     data: IStockUnitData[];
 }
 
-export interface IGetStockUnitResponse extends IResponse {
-    data: IStockUnitData;
-}
-
+export type IGetStockUnitResponse = IStockUnitRespone;
 export type ICreateStockUnitResponse = IGetStockUnitResponse;
-
 export type IEditStockUnitResponse = IGetStockUnitResponse;
+export type IDeleteStockUnitResponse = IGetStockUnitResponse;
