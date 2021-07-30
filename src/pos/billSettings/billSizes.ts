1ddfdedef96efa13334de1668352405f431db2ca
ThayalanGR <grthayalan18@gmail.com>
2021-07-30 05:57:01 +0530

    billSize enum changes

--- src/pos/billSettings/billSizes.ts
+++ src/pos/billSettings/billSizes.ts
@@ -1,6 +1,7 @@
+// intentionally assigned equivalent values in enum, in some type matching we need the billSizes name to be layed out the same
 export enum EBILL_SIZES {
-    BILL_A4,
-    BILL_90MM,
+    BILL_A4 = 'BILL_A4',
+    BILL_90MM = 'BILL_90MM',
 }
 
 export const getBillSizeByName = (billName: keyof typeof EBILL_SIZES): keyof typeof EBILL_SIZES =>
