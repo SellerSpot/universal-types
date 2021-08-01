734d91b18a85c6ee0f18bfecae61d620ebef983c
ThayalanGR <grthayalan18@gmail.com>
2021-08-01 18:49:45 +0530

    Sale and cart types added

---
+++ src/core/customer/customer.ts
@@ -0,0 +1,7 @@
+export interface ICustomerData {
+    id: string;
+    name: string;
+    email: string;
+    mobile: number;
+    addresss: number;
+}
--- src/pos/index.ts
+++ src/pos/index.ts
@@ -1,8 +1,13 @@
-// inventory routes
+// inventory
+export * from './inventory/outlet';
 export * from './inventory/request';
 
-// billSettings routes
+// billSettings
 export * from './billSettings/request';
 export * from './billSettings/response';
 export * from './billSettings/billSettings';
 export * from './billSettings/billSizes';
+
+// new sale / sale history
+export * from './saleHistory/request';
+export * from './saleHistory/response';
